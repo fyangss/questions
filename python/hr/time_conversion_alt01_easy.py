f7182f050b2e49c7001b92d48a867e6a64b3759d
Fred Yang <fredyang@google.com>
2020-01-02 01:32:18 -0800

    time conversion

---
+++ python/hr/time_conversion_alt01_easy.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import os
+import sys
+
+#
+# Complete the timeConversion function below.
+#
+def timeConversion(s):
+  time_of_day = s[-2:]
+  hour = s[:2]
+  time = s[2:-2]
+  if time_of_day == 'AM':
+    if hour == '12':
+      return '00' + time
+  else:
+    if hour != '12':
+      hour = str(int(hour) + 12)
+      return hour + time
+  return hour + time
+
+
+if __name__ == '__main__':
+    f = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = timeConversion(s)
+
+    f.write(result + '\n')
+
+    f.close()
+
