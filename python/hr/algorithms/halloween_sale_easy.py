c9da911f6d06f007e20ebc7d506fa1b4ecac280b
fyangss <fredyangss@gmail.com>
2020-01-19 18:41:10 -0800

    Create halloween_sale_easy.py

---
+++ python/hr/algorithms/halloween_sale_easy.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the howManyGames function below.
+def howManyGames(p, d, m, s):
+  cur_price = p
+  games = 0
+  while s >= cur_price:
+    s -= cur_price
+    games += 1
+    cur_price = max(cur_price - d, m)
+  return games
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    pdms = input().split()
+
+    p = int(pdms[0])
+
+    d = int(pdms[1])
+
+    m = int(pdms[2])
+
+    s = int(pdms[3])
+
+    answer = howManyGames(p, d, m, s)
+
+    fptr.write(str(answer) + '\n')
+
+    fptr.close()
